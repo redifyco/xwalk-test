632b49f24a5d588ff4212a40120b0c257dccd1ec
giacomovecchi <giacomo.vecchi1997@gmail.com>
2025-04-04 17:12:22 +0200

    Build Text and Background js

--- blocks/text-and-background/text-and-background.js
+++ blocks/text-and-background/text-and-background.js
@@ -2,44 +2,39 @@ import {isEditorMode} from "../../scripts/utils.js";
 
 export default async function decorate(block) {
   const inEditorMode = isEditorMode();
-  const isMobile = window.innerWidth < 1200
-  const backgroundImage = block.querySelector('div:first-child picture img') || ''
+  const backgroundImage = block.querySelector('div:first-child picture img') || '';
   const titleHTML = block.querySelector('div:nth-child(2) div').innerHTML || '';
   const descriptionHTML = block.querySelector('div:nth-child(3) div').innerHTML;
-  const buttonHTML = block.querySelector('div:nth-child(4) div p a');
-  console.log('backgroundImage', backgroundImage.src)
+  const buttonHTML = block.querySelector('div:nth-child(4) div p').innerHTML;
 
 
-  block.textContent = ''
+  block.textContent = '';
+
+  /*SECTION*/
+  const section = document.createElement('section');
+  section.className = 'bg-no-repeat bg-cover flex justify-end lg:justify-center bg-center px-4 lg:px-40 xl:px-64 2xl:px-80 py-40'
+  section.style.backgroundImage = `url(${backgroundImage.src})`;
 
 
-
-
-  /*DESKTOP*/
-  const desktopSection = document.createElement('section');
-  const dynamicTextColor = backgroundImage.src ? 'text-white' : 'text-current';
-  desktopSection.className = `bg-no-repeat bg-cover px-40 py-40 gap-16 flex justify-center items-center relative ${dynamicTextColor}`;
-  desktopSection.style.backgroundImage = `url(${backgroundImage.src})`;
-
+  /*CONTAINER TITLE*/
   const containerTitle = document.createElement('div');
-  containerTitle.className = 'w-1/2 uppercase text-6xl'
+  containerTitle.className = 'text-6xl w-full lg:text-8xl uppercase'
   containerTitle.innerHTML = titleHTML
-  desktopSection.appendChild(containerTitle)
 
-  const containerDescription = document.createElement('div')
-  containerDescription.className = 'flex flex-col translate-y-20 gap-4'
-  containerDescription.innerHTML = descriptionHTML
-  containerDescription.appendChild(buttonHTML)
-  desktopSection.appendChild(containerDescription)
+  /*CONTAINER DESCRIPTION + BUTTON*/
+  const containerDescription = document.createElement('div');
+  containerDescription.className = 'lg:translate-y-20 flex flex-col gap-10 lg:text-xl'
+  containerDescription.innerHTML = descriptionHTML + buttonHTML;
 
 
-  /*MOBILE*/
+  /*CONTAINER TEXT*/
+  const containerText = document.createElement('div');
+  containerText.className = 'text-end h-full lg:gap-32 w-2/3 lg:w-full gap-16  lg:text-start text-white justify-center lg:flex-row flex-col flex items-center'
+  containerText.appendChild(containerTitle)
+  containerText.appendChild(containerDescription)
 
-  const mobileSection = document.createElement('section');
-  mobileSection.innerHTML = '<h1>mobile</h1>'
+  section.appendChild(containerText)
 
 
-
-  isMobile ? block.appendChild(mobileSection) : block.appendChild(desktopSection)
-
+  block.appendChild(section);
 }
