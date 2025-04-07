f90748b05683ccfe287808ae198cfbde1ba0f29a
giacomovecchi <giacomo.vecchi1997@gmail.com>
2025-04-07 15:58:34 +0200

    Finish block - What drives us

--- blocks/text-and-background/text-and-background.js
+++ blocks/text-and-background/text-and-background.js
@@ -40,24 +40,24 @@ export default async function decorate(block) {
 
   /*SECTION*/
   const section = document.createElement('section');
-  section.className = `bg-no-repeat bg-cover flex justify-end lg:justify-center bg-center px-4 lg:px-40 xl:px-64 2xl:px-80 py-40 ${buildHeight}`
+  section.className = `bg-no-repeat bg-cover flex justify-end lg:justify-center bg-center lg:px-20 px-4 py-40 ${buildHeight}`
   section.style.backgroundImage = `url(${backgroundImage.src})`;
 
 
   /*CONTAINER TITLE*/
   const containerTitle = document.createElement('div');
-  containerTitle.className = 'text-6xl w-full lg:text-8xl uppercase'
+  containerTitle.className = 'text-6xl w-full lg:text-8xl xl:text-9xl uppercase'
   containerTitle.innerHTML = titleHTML
 
   /*CONTAINER DESCRIPTION + BUTTON*/
   const containerDescription = document.createElement('div');
-  containerDescription.className = 'lg:translate-y-20 flex flex-col gap-10 lg:text-xl'
+  containerDescription.className = 'lg:translate-y-20 leading-5 lg:leading-10 flex items-end lg:items-start flex-col gap-10 lg:text-xl'
   containerDescription.innerHTML = descriptionHTML;
   containerDescription.appendChild(createWhiteBorderButton(buttonObject.title, buttonObject.href))
 
   /*CONTAINER TEXT*/
   const containerText = document.createElement('div');
-  containerText.className = 'text-end h-full lg:gap-32 w-2/3 lg:w-full gap-16  lg:text-start text-white justify-center lg:flex-row flex-col flex items-center'
+  containerText.className = 'text-end h-full max-w-5xl w-3/4 lg:gap-32 lg:w-full gap-16  lg:text-start text-white justify-center lg:flex-row flex-col flex items-center'
   containerText.appendChild(containerTitle)
   containerText.appendChild(containerDescription)
 
