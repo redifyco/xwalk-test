b856f859719d0878f68164dbb5224a0a9899d6d6
giacomovecchi <giacomo.vecchi1997@gmail.com>
2025-04-07 16:10:05 +0200

    Finish block - What drives us v2

--- components/button.js
+++ components/button.js
@@ -1,23 +1,26 @@
-export function createWhiteBorderButton(label = 'Donate Now', href = '#') {
+export function createWhiteBorderButton(label = 'Donate Now', href = '#', themeColor = false) {
+
+
+  const animatedLinesColor = themeColor ? 'button-line-absolute-theme' : 'button-line-absolute-white';
 
   const link = document.createElement('a');
   link.href = href;
   link.className = 'group relative inline-block w-fit border border-white p-2 lg:border-0';
 
   const topLine = document.createElement('span');
-  topLine.className = 'button-line-absolute-white -top-1 -right-1 h-0.5 w-3/5 group-hover:w-9/12';
+  topLine.className = `-top-1 -right-1 h-0.5 w-3/5 group-hover:w-9/12 ${animatedLinesColor}`;
 
   const topRightLine = document.createElement('span');
-  topRightLine.className = 'button-line-absolute-white -top-1 -right-1 h-full w-0.5 group-hover:h-9/12';
+  topRightLine.className = `-top-1 -right-1 h-full w-0.5 group-hover:h-9/12 ${animatedLinesColor}`;
 
   const bottomLine = document.createElement('span');
-  bottomLine.className = 'button-line-absolute-white -bottom-1 -left-1 h-0.5 w-3/5 group-hover:w-9/12';
+  bottomLine.className = `-bottom-1 -left-1 h-0.5 w-3/5 group-hover:w-9/12 ${animatedLinesColor}`;
 
   const bottomLeftLine = document.createElement('span');
-  bottomLeftLine.className = 'button-line-absolute-white -bottom-1 -left-1 h-full w-0.5 group-hover:h-9/12';
+  bottomLeftLine.className = `-bottom-1 -left-1 h-full w-0.5 group-hover:h-9/12 ${animatedLinesColor}`;
 
   const text = document.createElement('span');
-  text.className = 'px-6 py-2 text-white';
+  text.className = `px-6 py-2 ${themeColor ? 'text-primary' : 'text-white'}`;
   text.textContent = label;
 
   // Appendi gli elementi
