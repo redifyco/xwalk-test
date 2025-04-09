49660d5cda1fbfe1f7c415d8fe9996c2f2da24eb
giacomovecchi <giacomo.vecchi1997@gmail.com>
2025-04-09 15:54:20 +0200

    MFNW-6 - Build Header block

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,5 +1,6 @@
 import {getMetadata} from '../../scripts/aem.js';
 import {loadFragment} from '../fragment/fragment.js';
+import {createWhiteBorderButton} from "../../components/button.js";
 
 
 export default async function decorate(block) {
@@ -7,58 +8,69 @@ export default async function decorate(block) {
   const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
   const fragment = await loadFragment(navPath);
 
+  console.log('fragment', fragment)
+  console.log('block', block)
+
   block.textContent = ''
 
 
   const logoImage = fragment.querySelector('picture img')
-  block.appendChild(logoImage)
+  if (logoImage) {
+    block.appendChild(logoImage)
+  }
 
-  const menuContainer = fragment.querySelector('.default-content-wrapper ul');
-  const subMenuStates = new Map();
+  const containerListMenu = fragment.querySelector('.default-content-wrapper ul');
+  if (containerListMenu) {
+    containerListMenu.className = 'flex gap-4'
+    const subMenuStates = new Map();
+    containerListMenu.querySelectorAll(':scope > li').forEach((menu) => {
+      const subMenu = menu.querySelector('ul');
+      subMenu.className = 'invisible absolute p-4 pt-10 text-lg bg-white shadow-lg flex flex-col gap-2 w-max';
+      const menuWrapper = document.createElement('div');
+      const menuTitle = menuWrapper.appendChild(menu.querySelector('p'));
+      menuTitle.className = 'text-white text-lg flex items-center after:mt-2 gap-2 after:size-4 after:bg-[url(/assets/icons/chevron-down-theme.svg)] after:flex after:bg-contain after:bg-no-repeat !font-medium cursor-pointer';
 
-  menuContainer.querySelectorAll(':scope > li').forEach((menu) => {
-    const subMenu = menu.querySelector('ul');
-    subMenu.className = 'invisible absolute p-4 pt-10 text-lg bg-white shadow-lg flex flex-col gap-2 w-max';
-    const menuWrapper = document.createElement('div');
-    const menuTitle = menuWrapper.appendChild(menu.querySelector('p'));
-    menuTitle.className = 'text-primary text-lg flex items-center after:mt-2 gap-2 after:size-4 after:bg-[url(/assets/icons/chevron-down-theme.svg)] after:flex after:bg-contain after:bg-no-repeat !font-medium cursor-pointer';
+      menuWrapper.appendChild(menuTitle);
+      menuWrapper.appendChild(subMenu);
 
-    menuWrapper.appendChild(menuTitle);
-    menuWrapper.appendChild(subMenu);
+      subMenuStates.set(menuTitle, false);
 
-    subMenuStates.set(menuTitle, false);
+      menuTitle.addEventListener("click", () => {
+        const isCurrentlyOpen = subMenuStates.get(menuTitle);
 
-    menuTitle.addEventListener("click", () => {
-      const isCurrentlyOpen = subMenuStates.get(menuTitle);
+        // Close all submenus
+        subMenuStates.forEach((_, title) => {
+          const targetMenu = title.nextElementSibling;
+          targetMenu.classList.remove('visible')
+          targetMenu.classList.add('invisible')
 
-      // Close all submenus
-      subMenuStates.forEach((_, title) => {
-        const targetMenu = title.nextElementSibling;
-        targetMenu.classList.remove('visible')
-        targetMenu.classList.add('invisible')
+          subMenuStates.set(title, false);
+        });
 
-        subMenuStates.set(title, false);
+        // Toggle clicked submenu
+        if (!isCurrentlyOpen) {
+          subMenu.classList.remove('invisible')
+          subMenu.classList.add('visible')
+          subMenu.querySelectorAll(':scope > li').forEach((item) => {
+            item.className = ' after:size-4 flex items-center gap-2 after:bg-[url(/assets/icons/chevron-right-black.svg)] after:bg-contain after:bg-no-repeat after:block'
+          })
+          subMenuStates.set(menuTitle, true);
+        }
       });
-
-      // Toggle clicked submenu
-      if (!isCurrentlyOpen) {
-        subMenu.classList.remove('invisible')
-        subMenu.classList.add('visible')
-        subMenu.querySelectorAll(':scope > li').forEach((item) => {
-          item.className = ' after:size-4 flex items-center gap-2 after:bg-[url(/assets/icons/chevron-right-black.svg)] after:bg-contain after:bg-no-repeat after:block'
-        })
-        subMenuStates.set(menuTitle, true);
-      }
+      containerListMenu.appendChild(menuWrapper);
     });
 
-    block.appendChild(menuWrapper);
-  });
+    block.appendChild(containerListMenu)
+  }
 
-
-  const buttonLink = fragment.querySelector('div:nth-child(4) div p a');
+  const buttonLink = fragment.querySelector('.default-content-wrapper > *:nth-last-child(2)')?.innerHTML;
+  const buttonText = fragment.querySelector('.default-content-wrapper > *:last-child')?.innerHTML;
+  if (buttonLink && buttonText) {
+    block.appendChild(createWhiteBorderButton(buttonText, buttonLink))
+  }
 
   /*WRAPPER*/
-  block.className = 'flex fixed items-center gap-2';
+  block.className = 'flex px-4 fixed w-full justify-between items-center gap-2';
 
   /*CONTAINER*/
 
