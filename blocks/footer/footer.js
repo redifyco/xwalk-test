db4d4ec9664504087464a5fd650615bc8d187048
giacomovecchi <giacomo.vecchi1997@gmail.com>
2025-04-14 10:13:52 +0200

    MFNW-15 - Created HTML

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,20 +1,69 @@
-import { getMetadata } from '../../scripts/aem.js';
-import { loadFragment } from '../fragment/fragment.js';
+import { getMetadata } from "../../scripts/aem.js";
+import { loadFragment } from "../fragment/fragment.js";
+import { getJsonFromHtml } from "../../scripts/utils.js";
 
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
-  // load footer as fragment
-  const footerMeta = getMetadata('footer');
-  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
-  const fragment = await loadFragment(footerPath);
+  const navMeta = getMetadata("footer");
+  const navPath = navMeta
+    ? new URL(navMeta, window.location).pathname
+    : "/footer";
+  const fragment = await loadFragment(navPath);
 
-  // decorate footer DOM
-  block.textContent = '';
-  const footer = document.createElement('div');
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+  const data = fragment.querySelector(".default-content-wrapper");
+  const darkLogo = data.querySelector("p:nth-child(1) img");
+  console.log("default-content-wrapper", data);
 
-  block.append(footer);
+    const facebookLink = fragment.querySelector('.default-content-wrapper  p:nth-child(3) a')?.href;
+    const instagramLink = fragment.querySelector('.default-content-wrapper  p:nth-child(4) a')?.href;
+    const linkedinLink = fragment.querySelector('.default-content-wrapper  p:nth-child(5) a')?.href;
+    const youtubeLink = fragment.querySelector('.default-content-wrapper  p:nth-child(6) a')?.href;
+
+  console.log("linkedinLink", linkedinLink);
+
+  const navigationListHTML = data.querySelector("ul");
+  const navigationListJson = getJsonFromHtml(navigationListHTML);
+
+  block.textContent = "";
+
+  const footerDiv = document.querySelector(".footer-wrapper");
+
+  // language=HTML
+  footerDiv.innerHTML = `
+		<div class="bg-primary px-4 lg:p-20 gap-10 w-full xl:flex-nowrap flex-wrap lg:flex-row lg:gap-20 flex items-center p-14  flex-col justify-center lg:justify-between text-white">
+			<div>
+				<img class="max-w-80"
+					src="${darkLogo.src}"
+					alt="">
+			</div>
+			<div class="flex w-full lg:max-w-none max-w-96 items-center lg:items-start flex-col gap-10 lg:flex-row lg:justify-between">
+				${navigationListJson
+          .map(
+            (item) =>
+              ` <ul class="flex flex-col justify-center lg:items-start items-center gap-2">
+                <h6 class="text-2xl hidden lg:block uppercase font-semibold">${item.title}</h6>
+${item.subItems
+                  .map(
+                    (subItem) => `
+                        <li class="text-xs py-1 lg:text-xl font-light">
+                            <a href="${subItem.href}">${subItem.title}</a>
+                        </li>`
+                  )
+                  .join(" ")}
+                </ul>`
+          )
+          .join(" ")}
+			</div>
+				<div class='text-white gap-2 flex items-center'>
+					<a href="${facebookLink}"><ion-icon size="large" name="logo-facebook"></ion-icon></a>
+					<a href="${instagramLink}"><ion-icon size="large" name="logo-instagram"></ion-icon></a>
+					<a href="${linkedinLink}"><ion-icon size="large" name="logo-linkedin"></ion-icon></a>
+					<a href="${youtubeLink}"><ion-icon size="large" name="logo-youtube"></ion-icon></a>
+				</div>
+		</div>`;
+
+  console.log("fragment", fragment);
 }
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,32 +1,64 @@
 export const isEditorMode = () => {
-  // Verifica sessionStorage per chiavi che iniziano con "aue"
-  for (let i = 0; i < sessionStorage.length; i++) {
-    const key = sessionStorage.key(i);
-    if (key && key.startsWith('aue')) {
-      return true;
+    // Verifica sessionStorage per chiavi che iniziano con "aue"
+    for (let i = 0; i < sessionStorage.length; i++) {
+        const key = sessionStorage.key(i);
+        if (key && key.startsWith('aue')) {
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 };
 
 
 export function classNames(classes) {
-  return Object.entries(classes)
-    .filter(([_, value]) => Boolean(value))
-    .map(([key]) => key)
-    .join(' ');
+    return Object.entries(classes)
+        .filter(([_, value]) => Boolean(value))
+        .map(([key]) => key)
+        .join(' ');
 }
 
 export function handleScrollClasses(node, threshold, cssClasses, removeOnScroll = true) {
-  const classes = Array.isArray(cssClasses) ? cssClasses : [cssClasses];
+    const classes = Array.isArray(cssClasses) ? cssClasses : [cssClasses];
 
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > threshold) {
-      node.classList.add(...classes);
-    } else if (removeOnScroll) {
-      node.classList.remove(...classes);
-    }
-  });
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > threshold) {
+            node.classList.add(...classes);
+        } else if (removeOnScroll) {
+            node.classList.remove(...classes);
+        }
+    });
 }
 
+
+export function getJsonFromHtml(htmlList) {
+    const result = [];
+
+    if (!htmlList || !(htmlList instanceof HTMLElement)) {
+        console.warn('Invalid HTML list provided.');
+        return result;
+    }
+
+    try {
+        htmlList.querySelectorAll(':scope > li').forEach((item) => {
+            const title = item.querySelector('p')?.textContent.trim() || 'Untitled';
+            const subItems = [];
+
+            item.querySelectorAll('ul > li > a').forEach((link) => {
+                subItems.push({
+                    href: link.getAttribute('href') || '#',
+                    title: link.getAttribute('title') || 'No title',
+                });
+            });
+
+            result.push({
+                title,
+                subItems,
+            });
+        });
+    } catch (error) {
+        console.error('Error parsing HTML list:', error);
+    }
+
+    return result;
+}
 
