bde8d93550aabfafa161e7d16c7672fed1824fce
giacomovecchi <giacomo.vecchi1997@gmail.com>
2025-05-13 17:27:56 +0200

    MFNW-6 - Improve Header v2

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -5,21 +5,23 @@ import {headerMobile} from "./mobile.js";
 
 
 export default async function decorate(block) {
-  const scrollThreshold = 100;
-  const navMeta = getMetadata('nav');
-  const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
-  const fragment = await loadFragment(navPath);
+    console.log('block', block)
+    const scrollThreshold = 100;
+    const navMeta = getMetadata('nav');
+    const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
+    const fragment = await loadFragment(navPath);
 
-  const darkLogo = fragment.querySelector('p:nth-child(1) img');
-  const lightLogo = fragment.querySelector('p:nth-child(2) img');
-  const containerListMenu = fragment.querySelector('.default-content-wrapper ul');
+    const darkLogo = fragment.querySelector('p:nth-child(1) img');
+    const lightLogo = fragment.querySelector('p:nth-child(2) img');
+    const containerListMenu = fragment.querySelector('.default-content-wrapper ul');
 
-  const desktopMenu = containerListMenu.cloneNode(true)
-  const mobileMenu = containerListMenu.cloneNode(true)
+    const desktopMenu = containerListMenu.cloneNode(true)
+    const mobileMenu = containerListMenu.cloneNode(true)
 
-  block.textContent = ''
+    // block.textContent = ''
 
-  block.appendChild(headerDesktop(block, fragment, scrollThreshold, darkLogo, lightLogo, desktopMenu))
-  block.appendChild(headerMobile(block, fragment, scrollThreshold, darkLogo, lightLogo, mobileMenu))
+    // block.appendChild(headerDesktop(block, fragment, scrollThreshold, darkLogo, lightLogo, desktopMenu))
+    // block.appendChild(headerMobile(block, fragment, scrollThreshold, darkLogo, lightLogo, mobileMenu))
+
 
 }
