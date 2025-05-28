60c58779a3815d5aa46fcde22175334f805ea805
giacomovecchi <giacomo.vecchi1997@gmail.com>
2025-05-28 11:27:23 +0200

    MFNW-76 - Implement Advanced Adyen form v8

--- scripts/adyen-init.js
+++ scripts/adyen-init.js
@@ -3,53 +3,62 @@ const {AdyenCheckout, Card} = window.AdyenWeb;
 
 export const initDonationForm = (data) => {
     console.log('click init donation form', data);
-    fetch('/bin/msc-foundation/services/adyen?type=GET_PAYMENT_METHODS', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    }).then(res => res.json())
-        .then(async (paymentMethods) => {
-            const parsePaymentMethods = JSON.parse(paymentMethods.data);
+    Promise.all([
+        fetch('/bin/msc-foundation/services/adyen?type=CREATE_SESSION', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(res => res.json()),
+        fetch('/bin/msc-foundation/services/adyen?type=GET_PAYMENT_METHODS', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(res => res.json())
+    ]).then(async ([session, paymentMethods]) => {
 
-            const configuration = {
-                paymentMethodsResponse: parsePaymentMethods,
-                clientKey: "test_6HJJXDTT5BHWJEIQQJPMNDVQW4VBAMI6",
-                locale: 'it-IT',
-                countryCode: 'IT',
-                environment: 'test',
-                amount: {
-                    value: data.value,
-                    currency: data.currency
-                },
-                onSubmit: async (result, component) => {
-                    console.log('submit', result, component);
-                },
-                onAdditionalDetails: async (state, component, actions) => {
-                    console.log('additional details', state, component, actions);
-                },
-                onPaymentCompleted: (result, component) => {
-                    console.info(result, component);
-                },
-                onPaymentFailed: (result, component) => {
-                    console.info(result, component);
-                },
-                onError: (error, component) => {
-                    console.error(error.name, error.message, error.stack, component);
-                },
-                onChange: (state, component) => {
-                    console.log('change', state, component);
-                },
-            }
+        console.log('session', session);
+        console.log('paymentMethods', paymentMethods);
+        const configuration = {
+            paymentMethodsResponse: paymentMethods,
+            clientKey: "test_6HJJXDTT5BHWJEIQQJPMNDVQW4VBAMI6",
+            locale: 'it-IT',
+            countryCode: 'IT',
+            environment: 'test',
+            amount: {
+                value: data.value,
+                currency: data.currency
+            },
+            onSubmit: async (result, component) => {
+                console.log('submit', result, component);
+            },
+            onAdditionalDetails: async (state, component, actions) => {
+                console.log('additional details', state, component, actions);
+            },
+            onPaymentCompleted: (result, component) => {
+                console.info(result, component);
+            },
+            onPaymentFailed: (result, component) => {
+                console.info(result, component);
+            },
+            onError: (error, component) => {
+                console.error(error.name, error.message, error.stack, component);
+            },
+            onChange: (state, component) => {
+                console.log('change', state, component);
+            },
+        }
 
-            const checkout = await AdyenCheckout(configuration);
-            const card = new Card(checkout).mount('#card-container');
-            console.log('configuration', configuration);
-            console.log('checkout', checkout);
+        const checkout = await AdyenCheckout(configuration);
+        const card = new Card(checkout).mount('#card-container');
+        console.log('configuration', configuration);
+        console.log('checkout', checkout);
 
 
-        })
+    })
         .catch(err => console.log('error', err));
 }
 
