2c46f2602ecb9cd661a93a44b591503c10d2b2d4
giacomovecchi <giacomo.vecchi1997@gmail.com>
2025-05-30 17:43:36 +0200

    MFNW-76 - clean code

--- scripts/adyen-init.js
+++ scripts/adyen-init.js
@@ -2,10 +2,10 @@ const {AdyenCheckout, Dropin} = window.AdyenWeb;
 
 const ADYEN_CLIENT_KEY_TEST = 'test_4TAQ4FQCQFGWVOH5XB3SHGF4YQUKNJMQ';
 const ADYEN_CLIENT_KEY_PROD = 'test_RECMBL5SWRCZBBE2OLQ57DZNNAK74JHQ';
+const ENVIRONMENT = 'test';
 
 
 export const initDonationForm = (data, onPaymentCompleted, onPaymentFailed) => {
-    console.log('click init donation form', data);
     Promise.all([
         fetch('/api/msc-foundation/services/adyen?type=CREATE_SESSION', {
             method: 'POST',
@@ -22,10 +22,8 @@ export const initDonationForm = (data, onPaymentCompleted, onPaymentFailed) => {
             body: JSON.stringify(data)
         }).then(res => res.json())
     ]).then(async ([session, paymentMethods]) => {
-
-        console.log('session', session);
-        console.log('paymentMethods', paymentMethods);
         const parsedSession = JSON.parse(session.data);
+        const parsedPaymentMethods = JSON.parse(paymentMethods.data);
 
         const globalConfiguration = {
             session: {
@@ -36,8 +34,8 @@ export const initDonationForm = (data, onPaymentCompleted, onPaymentFailed) => {
                 value: data.amount,
                 currency: data.currency
             },
-            environment: 'test', // Change to 'live' for the live environment.
-            paymentMethodsResponse: JSON.parse(paymentMethods.data),
+            environment: ENVIRONMENT,
+            paymentMethodsResponse: parsedPaymentMethods,
             locale: 'it-IT',
             countryCode: 'IT',
             clientKey: ADYEN_CLIENT_KEY_PROD,
@@ -59,11 +57,6 @@ export const initDonationForm = (data, onPaymentCompleted, onPaymentFailed) => {
 
         const checkout = await AdyenCheckout(globalConfiguration);
         const dropin = new Dropin(checkout, dropinConfiguration).mount('#dropin-container');
-
-        console.log('checkout', checkout);
-        console.log('dropin', dropin);
-
-
     })
         .catch(err => console.log('error', err));
 }
