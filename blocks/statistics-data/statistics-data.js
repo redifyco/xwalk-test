47ed8e17675cbc631794f17c6d4fc07ed3fc62ef
giacomovecchi <giacomo.vecchi1997@gmail.com>
2025-06-05 08:40:29 +0200

    Add background condition to statistics-data.js

--- blocks/statistics-data/statistics-data.js
+++ blocks/statistics-data/statistics-data.js
@@ -10,6 +10,8 @@ export default function decorate(block) {
     const statsList = Array.from(block.querySelectorAll(":scope > div:nth-child(n+7)"));
     const JSONStatisticsData = processDivsToObjectStatisticsData(statsList);
 
+    console.log('backgroundImage', !!backgroundImage)
+
     const containerSection = document.createElement("section");
     containerSection.className = `
     flex flex-col gap-14 bg-cover bg-center justify-center bg-no-repeat py-30
@@ -20,15 +22,15 @@ export default function decorate(block) {
 
     containerSection.innerHTML = `
     <div class="flex flex-col items-center lg:items-start">
-      <h2 class="relative block px-4 text-center text-3xl font-semibold text-white uppercase lg:px-16 lg:text-start lg:text-6xl 2xl:text-7xl">
+      <h2 class="relative block px-4 text-center text-3xl font-semibold uppercase lg:px-16 lg:text-start lg:text-6xl 2xl:text-7xl ${backgroundImage ? 'text-white' : 'text-primary'}">
         ${title}
-        <span class="absolute left-0 mt-5 hidden border-b-2 border-b-white lg:block lg:w-[50%] xl:w-[90%]"></span>
+        <span class="absolute left-0 mt-5 hidden border-b-2 lg:block lg:w-[50%] xl:w-[90%] ${backgroundImage ? 'border-b-white' : 'border-b-primary'}"></span>
       </h2>
     </div>
 
     <div class="grid grid-cols-2 justify-items-center gap-8 px-4 lg:justify-items-start lg:gap-24 lg:px-24 xl:gap-32 xl:px-32">
       ${JSONStatisticsData.length > 0 ? JSONStatisticsData.map((item) => `
-        <div class="flex flex-col items-center text-white lg:flex-row lg:items-end lg:gap-6">
+        <div class="flex flex-col items-center  lg:flex-row lg:items-end lg:gap-6 ${backgroundImage ? 'text-white' : 'text-primary'}">
           <h6 class="text-6xl md:text-7xl leading-tight font-semibold 2xl:text-9xl 2xl:leading-24">
             ${item.value}
           </h6>
