3d04ca0d468a21c41606e6445c9c5f3998d161c2
giacomovecchi <giacomo.vecchi1997@gmail.com>
2025-06-06 09:53:07 +0200

    Fix text-hero-subtitle.js

--- blocks/text-hero-subtitle/text-hero-subtitle.js
+++ blocks/text-hero-subtitle/text-hero-subtitle.js
@@ -3,8 +3,8 @@ export default function decorate(block) {
         backgroundImage: block.querySelector(":scope > div:nth-child(1) div img")?.src || "",
         heroTitle: block.querySelector(":scope > div:nth-child(2) p")?.textContent || "",
         isCenteredBox: block.querySelector(":scope > div:nth-child(3) p")?.textContent === 'true',
-        headingSubtitleBox: block.querySelector(":scope > div:nth-child(4) p")?.textContent || '',
-        contentSubtitleBox: block.querySelector(":scope > div:nth-child(5)") || ''
+        headingSubtitleBox: block.querySelector(":scope > div:nth-child(4)")?.innerHTML || '',
+        contentSubtitleBox: block.querySelector(":scope > div:nth-child(5)")?.innerHTML || ''
     };
 
 
@@ -22,9 +22,9 @@ export default function decorate(block) {
                 </h2>
                 <div class="absolute inset-0 bg-black/20"></div>
             </div>
-            <div class="container-layout-padding flex ${data.isCenteredBox ? 'flex-col text-center items-center' : 'flex-row justify-between'} mx-auto">
-                ${data.headingSubtitleBox && `<h3 class="text-7xl text-primary font-medium mb-8">${data.headingSubtitleBox}</h3>`}
-                ${data.contentSubtitleBox.innerHTML && `<div class="${data.isCenteredBox ? 'w-9/12 italic text-2xl' : 'w-8/12 text-xl'}">${data.contentSubtitleBox.innerHTML}</div>`}
+            <div class="container-layout-padding flex ${data.isCenteredBox ? 'flex-col text-center items-center' : 'flex-col lg:flex-row items-center text-center lg:items-start lg:text-start lg:justify-between'} mx-auto">
+                ${data.headingSubtitleBox && `<div class="text-7xl text-primary prose-em:font-joyful font-medium mb-8">${data.headingSubtitleBox}</div>`}
+                ${data.contentSubtitleBox && `<div class="${data.isCenteredBox ? 'w-9/12 italic text-2xl' : 'w-8/12 text-xl'}">${data.contentSubtitleBox}</div>`}
             </div>
         </div>
     `;
