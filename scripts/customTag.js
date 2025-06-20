1cf42099862b049739bdd233d3c1ff1677d8db20
giacomovecchi <giacomo.vecchi1997@gmail.com>
2025-06-20 11:16:32 +0200

    Fix grid blog-preview-filter.js v7

--- scripts/customTag.js
+++ scripts/customTag.js
@@ -214,7 +214,7 @@ export class ArticleCard extends HTMLElement {
       : `<a href="${href || '#'}">`;
 
     this.innerHTML = `
-      <div class="min-w-80 max-w-full sm:max-w-80 lg:max-w-[350px] xl:max-w-[400px]">
+      <div class="min-w-72 max-w-full sm:max-w-72 lg:max-w-[350px] xl:max-w-[400px]">
         ${linkOpen}
           <div class="relative">
             ${renderImage()}
