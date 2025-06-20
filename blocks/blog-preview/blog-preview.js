9a9494224a09b28533ba34c9efa329f701b0caf7
giacomovecchi <giacomo.vecchi1997@gmail.com>
2025-06-20 12:22:25 +0200

    Roll back blog-preview.js

--- blocks/blog-preview/blog-preview.js
+++ blocks/blog-preview/blog-preview.js
@@ -33,13 +33,12 @@ export default function decorate(block) {
     sectionContainer.className = ''
 
     sectionContainer.innerHTML = `
-      <section class="flex flex-col items-center justify-center gap-8 container-layout-padding">
+      <section class="flex flex-col items-center justify-center gap-8 px-4 pb-14 lg:gap-32 lg:px-20 lg:py-14">
         <h2 class="text-primary text-center text-3xl uppercase lg:text-7xl">
           ${title}
         </h2>
         <!--RELATED PROGRAMMES-->
-        <div 
-        class="grid grid-cols-2 lg:grid-cols-3 gap-4">
+        <div class="scrollbar scrollbar-thumb-primary scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-track-gray-300 flex max-w-full justify-start gap-6 overflow-x-scroll pb-5 lg:flex-wrap lg:justify-center">
           ${mockCardsData.length > 0 && mockCardsData.map((item, index) => `
             <article-card 
               variant="${cardStyle}"
